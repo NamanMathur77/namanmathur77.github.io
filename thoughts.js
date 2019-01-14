154f7e6f601ba528cb6f14c155a5fe2a5c26a42b
NamanMathur <36097162+NamanMathur77@users.noreply.github.com>
2019-01-14 13:01:55 +0530

    adding changes

---
+++ thoughts.js
@@ -0,0 +1,8 @@
+var thg=document.getElementById("th1");
+window.onscroll=function(){
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    thg.innerHTML="<b><u>All this looking back is a pain in your neck</u></b>";
+    setTimeout(function(){
+      thg.innerHTML="<b><u>\"One Life no Regeret\"</u></b>";
+    },5000);
+  }};
